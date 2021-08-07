e16ec428f1d2dc35a68e8d853397577d9a4438de
Aly Nasser <ng.alynasser@gmail.com>
2021-08-08 01:26:27 +0200

    feat: validate selected bookable

--- src/components/Bookables/BookablesView.js
+++ src/components/Bookables/BookablesView.js
@@ -1,12 +1,20 @@
-import { Fragment, useState } from "react";
+import { Fragment, useCallback, useState } from "react";
 import BookablesList from "./BookablesList";
 import BookablesDetails from "./BookablesDetails";
 
 export default function BookablesView() {
   const [bookable, setBookable] = useState();
+
+  const updateBookable = useCallback((selected) => {
+    if (selected) {
+      selected.dateShown = new Date();
+      setBookable(selected);
+    }
+  }, []);
+
   return (
     <Fragment>
-      <BookablesList bookable={bookable} setBookable={setBookable} />
+      <BookablesList bookable={bookable} setBookable={updateBookable} />
       <BookablesDetails bookable={bookable} />
     </Fragment>
   );
